914774e92e6ff847af1c0cb6f1b87deb1e4efcc0
Enric Pina <epina87@gmail.com>
2022-09-01 10:06:15 +0200

    rebote y puntos

--- entidades.py
+++ entidades.py
@@ -1,3 +1,4 @@
+from random import randint
 import pygame as pg
 class Bola:
     def __init__(self, center_x, center_y,radio=10, color=(255,255,0)):
@@ -7,16 +8,54 @@ class Bola:
         self.radio  = radio
 
         self.vx = 0
-        self.vt = 0
+        self.vy = 0
 
     def dibujar(self,pantalla):
         pg.draw.circle(pantalla,self.color,(self.center_x,self.center_y), self.radio)
    
+    def mover(self,x_max= 800 , y_max =600):
+        self.center_x += self.vx
+        self.center_y += self.vy
+        punto = "0"
+
+        if self.center_y  >= y_max - self.radio or self.center_y < self.radio :
+            self.vy = self.vy * -1
+
+        if self.center_x >=x_max or self.center_x<0:
+            if self.center_x >=x_max:
+                punto = "r1"                
+            elif self.center_x<0:
+                punto = "r2"
+                
+            self.center_x = x_max // 2
+            self.center_y = y_max // 2 
+            
+            self.rebotar()            
+        return punto
+            
+    def rebotar(self):
+ 
+            self.vy  = -5
+            self.vy *= randint(-1,1)  
+            self.vx *=  -1  
+
+    @property
+    def izquierda(self):
+        return self.center_x - self.radio
+
+    @property
+    def derecha(self):
+        return self.center_x + self.radio
+
+    @property
+    def arriba(self):
+        return self.center_y - self.radio
+
+    @property
+    def abajo(self):
+        return self.center_y + self.radio
 
 class Raqueta:
-    """
-
-    """
     def __init__(self, center_x, center_y , w=120,h =20, color=(255,255,0)) -> None:
         self.center_x = center_x
         self.center_y = center_y 
@@ -26,6 +65,8 @@ class Raqueta:
 
         self.vx = 0
         self.vt = 0    
+
+        self.puntos = 0
 
     def dibujar(self,pantalla):
         pg.draw.rect(pantalla,self.color,(self.center_x - self.w//2, self.center_y - self.h // 2, self.w, self.h))
@@ -41,3 +82,20 @@ class Raqueta:
             self.center_y += self.vy        
         if self.center_y > y_max - self.h //2:
             self.center_y = y_max - self.h //2
+
+    @property
+    def izquierda(self):
+        return self.center_x - self.w //2
+
+    @property
+    def derecha(self):
+        return self.center_x + self.w //2
+
+    @property
+    def arriba(self):
+        return self.center_y - self.h //2
+
+    @property
+    def abajo(self):
+        return self.center_y + self.h //2
+
--- main.py
+++ main.py
@@ -1,33 +1,84 @@
 import pygame as pg
 from entidades import Bola,Raqueta
 
+#Inicio
 pg.init()
-
+#Crear Pantalla
 pantalla_principal = pg.display.set_mode((800,600))
+#Titulo Juego
 pg.display.set_caption("Pong ")
+
+#Guardar clase tiempo
 cronometro = pg.time.Clock()
 
+#Crear Var. Control Salida Juego
 game_over = False
+
+#Crear Bola
 bola = Bola(400,300,color=(255,255,255))
+#Asignar Velocidad Bola
+bola.vx = 5
+bola.vy = -5
+
+#Crear Raqueta
 raqueta1 = Raqueta(20,300,w=20,h=120,color=(255,255,255))
 raqueta2 = Raqueta(780,300,w=20,h=120,color=(255,255,255))
+#Asignar Velocidad Raqueta
 raqueta2.vy = 5
 raqueta1.vy = 5
 
+#Crear Texto Player
+miFuente = pg.font.Font(None,20)
+texto_jugador1 = miFuente.render("Player-1",0,(200,60,80)) 
+texto_jugador2 = miFuente.render("Player-2",0,(200,60,80)) 
+
+
+#Bucle Juego
 while not game_over:
+    # Control de tiempo 
     dt = cronometro.tick(60)
-    #Eventos que hace el usuario y devuelve una lista de eventos
+    #Gestión Eventos
     for evento in pg.event.get():
+        #Salir Juego
         if evento.type == pg.QUIT:
             game_over = True
 
+    #Mover Raqueta
     raqueta2.mover(pg.K_UP,pg.K_DOWN)
     raqueta1.mover(pg.K_w,pg.K_s)
+
+    #Mover Bola y retorna punto
+    punto  = bola.mover()
+    
+    #Control Rebote Raqueta
+    if bola.derecha >= raqueta2.izquierda and bola.abajo>= raqueta2.arriba and bola.arriba<= raqueta2.abajo:
+        bola.rebotar()
+
+    elif bola.izquierda <= raqueta1.derecha and bola.abajo>= raqueta1.arriba and bola.arriba<= raqueta1.abajo:
+        bola.rebotar() 
+    
+    #Guardar Puntos
+    if punto == "r1":
+        raqueta1.puntos += 1 
+    elif punto == "r2":    
+        raqueta2.puntos += 1 
+    Puntos_raqueta2 = miFuente.render(str(raqueta2.puntos),0,(200,60,80))
+    Puntos_raqueta1 = miFuente.render(str(raqueta1.puntos),0,(200,60,80))
+
+    #Mostrar Objetos
     pantalla_principal.fill((0,0,0))
     bola.dibujar(pantalla_principal)
     raqueta1.dibujar(pantalla_principal)
     raqueta2.dibujar(pantalla_principal)
 
-    #Le pasa la información a la tarjeta gráfica.            
+    #Mostrar Player
+    pantalla_principal.blit(texto_jugador1,(100,10))
+    pantalla_principal.blit(texto_jugador2,(700,10))
+
+    #Mostrar Puntos
+    pantalla_principal.blit(Puntos_raqueta1,(100,500))
+    pantalla_principal.blit(Puntos_raqueta2,(700,500))
+
+    #Envió Tarjeta Gráfica
     pg.display.flip()
 
